f9ad8d94b4f950b5160e655e55f11ebabc854b97
dganjali <64025829+dganjali@users.noreply.github.com>
2025-08-16 04:20:01 -0400

    deleted frontend

--- frontend/metro.config.js
+++
@@ -1,9 +0,0 @@
-const { getDefaultConfig } = require('expo/metro-config');
-
-const config = getDefaultConfig(__dirname);
-
-// Add SVG support
-config.resolver.assetExts.push('svg');
-config.transformer.babelTransformerPath = require.resolve('react-native-svg-transformer');
-
-module.exports = config;
